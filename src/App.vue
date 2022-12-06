20c7aec6b9895516876f7e8d592dc46ff25764b9
EdouardQ <edouard.quilliou@gmail.com>
2022-12-07 00:08:22 +0100

    add home - router - sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import Sidebar from "./components/Sidebar.vue"
+import {RouterView} from "vue-router"
 </script>
+
+<template>
+  <Sidebar />
+  <div class="container">
+    <RouterView />
+  </div>
+</template>
 
 <style>
 #app {
@@ -21,6 +17,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/front/Home.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="d-flex">
+    <h1>TEST</h1>
+  </div>
+</template>
+
+<script setup>
+</script>
